a3ba2bc01f9334323ca414663b10da6a516260b6
Calvin <calvin.chan.h@gmail.com>
2014-01-25 08:59:16 -0800

    Fixed routes?

--- freemail/urls.py
+++ freemail/urls.py
@@ -12,7 +12,10 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
-    url(r'^sendConf/(?P<email>\w+)', views.sendConf, name='sendConf')
+    url(r'^sendConf/(?P<email>\w+)', views.sendConf, name='sendConf'),
     url(r'^new_user/(?P<gmail>\w+)/(?P<fb>\w+)', views.addUser, name='addUser'),
     url(r'^getAllUsers$', views.getAllUsers, name='getAllUsers'),
+    #serves all other static files
+    url(r'^(?P<path>\.css)$', 'django.views.static.serve', {'document_root': '/public'}),
+    url(r'^(?P<path>\.js)$', 'django.views.static.serve', {'document_root': '/public'}),
 )
