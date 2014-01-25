299ea3f0b1668c98b59a2ae0110002e12400bf47
Jeff Wang <jeffreywang93@gmail.com>
2014-01-25 05:34:33 -0800

    halloo aman

--- freemail/views.py
+++ freemail/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+from pymongo import MongoClient
+client = MongoClient()
+db = client.freemail_database
+gmails = db.gmails
+new_gmail = { u'jeffreywang93' : u'ooJeffree@facebook.com'}
+
+gmails_id = gmails.insert(new_gmail)
+
+print(gmails_id)
+print(gmails.find_one())
 
 def index(request):
     return HttpResponse("Hello Werld")
+
+def addUser(request, gmail, fb):
+    return HttpResponse("Gmail is: " + gmail + ". Facebook is: " + fb)
