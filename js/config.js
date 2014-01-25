d5d41a840f82ffde3d64d15c6e59d156bb0ceeb4
Calvin <calvin.chan.h@gmail.com>
2014-01-25 08:34:42 -0800

    Signup done

--- js/config.js
+++ js/config.js
@@ -3,6 +3,19 @@ angular.module('freemail').config(['$routeProvider',
         $routeProvider.
         when('/', {
             templateUrl: '/views/index.html'
+        }).
+        when('/confirmation-sent', {
+            templateUrl: '/views/confirmation-sent.html'
+        }).
+        otherwise({
+            redirectTo: '/'
         });
+    }
+]);
+
+angular.module('freemail').config(['$locationProvider',
+    function($locationProvider) {
+        $locationProvider.html5Mode(true);
+        $locationProvider.hashPrefix("!");
     }
 ]);
