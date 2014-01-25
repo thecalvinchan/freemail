08e06fd954de9aedbfe49ec2e5be01e12e19a957
Calvin <calvin.chan.h@gmail.com>
2014-01-25 08:41:11 -0800

    Added front end files. Haven't merged yet though

--- js/controllers/signup.js
+++
@@ -1,9 +0,0 @@
-angular.module('freemail').controller('SignupController', ['$scope','$location',
-    function ($scope,$location) {
-        $scope.email;
-        $scope.submit = function() {
-            console.log($scope.email);
-            $location.path('confirmation-sent');
-        }
-    }
-]);
