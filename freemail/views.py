18e215dc26a4077c94697c2299c558c617b5da1d
Aman Agarwal <aman.agarwal.2718@gmail.com>
2014-01-25 08:32:20 -0800

    also for confirmation, doesn't work yet

--- freemail/views.py
+++ freemail/views.py
@@ -3,6 +3,9 @@ from pymongo import MongoClient
 client = MongoClient()
 db = client.freemail_database
 
+import os
+SALT = os.environ.get('DJANGO_SALT')
+
 def index(request):
     return HttpResponse("Hello Werld")
 
@@ -12,3 +15,15 @@ def addUser(request, gmail, fb):
                   "facebook" : fb }
     emails.insert(new_email)
     return HttpResponse("Gmail is: " + gmail + ". Facebook is: " + fb)
+
+def getAllUsers(request):
+    all_emails = ["facebook: '" + email[u'facebook'] + "' and gmail: '" + email[u'gmail'] + "'"
+        for email in db.emails.find()]
+    return HttpResponse('\n\n'.join(all_emails))
+    
+def sendConf(request, email):
+    confs = db.confs
+    new_conf = { "email" : email,
+                 "hash"  : generate_hash(email + SALT)}
+    confs.insert(new_conf)
+    return HttpResponse('Confirmation page created')
