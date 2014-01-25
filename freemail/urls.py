8cabe97a59611b87c0d184c84b26fa89100c0526
Calvin <calvin.chan.h@gmail.com>
2014-01-25 13:09:25 -0800

    Changed routes to match dash

--- freemail/urls.py
+++ freemail/urls.py
@@ -20,7 +20,7 @@ urlpatterns = patterns('',
     #serves all other static files
 
     # url(r'^(?P<path>css/\w+.css)$', views.testPath, name = 'testPath'),
-    url(r'^(?P<path>(\w+\/)*(\w+\.)+\w+)$', 'django.views.static.serve', {'document_root': 'public'}),
+    url(r'^(?P<path>((\w|\-)+\/)*((\w|\-)+\.)+(\w|\-)+)$', 'django.views.static.serve', {'document_root': 'public'}),
     url(r'', views.index, name='index'),
     #url(r'^(?P<path>js/\w+.js)$', 'django.views.static.serve', {'document_root': 'public'}),
     # url(r'^(?P<path>\.css)$', 'django.views.static.serve', {'document_root': '/public'}),
