1c9dd27fdaed0d12e7ea42fe9fbe448687995074
Aman Agarwal <aman.agarwal.2718@gmail.com>
2014-01-25 08:32:09 -0800

    for confirmation page

--- freemail/urls.py
+++ freemail/urls.py
@@ -12,5 +12,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
-    url(r'^new_user/(?P<gmail>\w+)/(?P<fb>\w+)', views.addUser, name='addUser')
+    url(r'^sendConf/(?P<email>\w+)', views.sendConf, name='sendConf')
+    url(r'^new_user/(?P<gmail>\w+)/(?P<fb>\w+)', views.addUser, name='addUser'),
+    url(r'^getAllUsers$', views.getAllUsers, name='getAllUsers'),
 )
