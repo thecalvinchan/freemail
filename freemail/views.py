7f3bb148757d331fa7da073c5c2b8193bc58fd27
Aman Agarwal <aman.agarwal.2718@gmail.com>
2014-01-25 21:29:06 -0800

    this is wrong but ill fix at merge

--- freemail/views.py
+++ freemail/views.py
@@ -1,26 +1,26 @@
-from __future__ import print_function
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
 from django.shortcuts import render, redirect, render_to_response
 from django.core.context_processors import csrf
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.csrf import ensure_csrf_cookie
+# from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import *
+# from django.views.decorators.csrf import ensure_csrf_cookie
+# from django.views.decorators.csrf import csrf_response_exempt
 from pymongo import MongoClient
 from django.core.mail import send_mail
 import datetime
 import random
 import string
 import json
-
 import os
+import hashlib
+
 SALT = os.environ.get('DJANGO_SALT')
 MONGO_URI = os.environ.get('MONGO_URI') 
 client = MongoClient(MONGO_URI)
 db = client.freemail_database
 
 ALPHABET = string.ascii_letters + string.digits
-
-import hashlib
 
 @ensure_csrf_cookie
 def index(request):
@@ -29,29 +29,29 @@ def index(request):
     c.update(csrf(request))
     return render(request,'index.html',c)
 
-def addUser(request, gmail, fb):
-    emails = db.emails
-    new_email = { "gmail" : gmail,
-                  "facebook" : fb }
-    emails.insert(new_email)
-    return HttpResponse("Gmail is: " + gmail + ". Facebook is: " + fb)
+# def addUser(request, gmail, fb):
+#     emails = db.emails
+#     new_email = { "gmail" : gmail,
+#                   "facebook" : fb }
 
-def getAllUsers(request):
-    all_emails = ["facebook: '" + email[u'facebook'] + "' and gmail: '" + email[u'gmail'] + "'"
-        for email in db.emails.find()]
-    return HttpResponse('\n\n'.join(all_emails))
+#     return HttpResponse("Gmail is: " + gmail + ". Facebook is: " + fb)
+
+# def getAllUsers(request):
+#     all_emails = ["facebook: '" + email[u'facebook'] + "' and gmail: '" + email[u'gmail'] + "'"
+#         for email in db.emails.find()]
+#     return HttpResponse('\n\n'.join(all_emails))
     
-def sendConf(request):
-    confs = db.confs
-    new_conf = { "email" : email,
-                 "hash"  : hashlib.sha1(email + SALT).hexdigest() }
-    confs.insert(new_conf)
-    send_mail('[FreeMail] Email Confirmation',
-              'Confirm your account by clicking on the following link: ' + '<a href="localhost:5000/confirm/' + email + '/' + new_conf["hash"] + '">Here</a>',
-              'test@freemail.bymail.in',
-              [email],
-              fail_silently=False)
-    return HttpResponse('Confirmation page created')
+# def sendConf(request):
+#     confs = db.confs
+#     new_conf = { "email" : email,
+#                  "hash"  : hashlib.sha1(email + SALT).hexdigest() }
+#     confs.insert(new_conf)
+#     send_mail('[FreeMail] Email Confirmation',
+#               'Confirm your account by clicking on the following link: ' + '<a href="localhost:5000/confirm/' + email + '/' + new_conf["hash"] + '">Here</a>',
+#               'test@freemail.bymail.in',
+#               [email],
+#               fail_silently=False)
+#     return HttpResponse('Confirmation page created')
 
 def generate_hash(input_str):
     return hashlib.sha1(input_str).hexdigest()
@@ -71,7 +71,7 @@ def confirmation(request):
         new_conf = { "email" : email,
                      "date" : datetime.datetime.utcnow(),
                      "hash"  : generate_hash(password + generate_salt())
-	}
+        }
         confs.insert(new_conf)
         send_mail('[FreeMail] Email Confirmation',
                   'Confirm your account by clicking on the following link: ' + 'http://localhost:5000/confirm/?email=' + email + '&id=' + new_conf["hash"],
@@ -79,6 +79,7 @@ def confirmation(request):
                   [email],
                   fail_silently=False)
         return HttpResponse(json.dumps({"email": email, "success": True}), content_type="application/json")
+<<<<<<< HEAD
     return HttpResponse(json.dumps({"email": email, "success": False}), content_type="application/json",status=500)
 
 def recieveEmailINTHEASS(request):
@@ -87,14 +88,14 @@ def recieveEmailINTHEASS(request):
     return HttpResponse("All Good")
 
 @csrf_exempt
+=======
+    else:
+        return HttpResponse(json.dumps({"email": email, "success": False}), content_type="application/json",status=500)
+
+@csrf_response_exempt
+>>>>>>> this is wrong but ill fix at merge
 def inbound(request):
-    print(request.method)
-    print request
-    # tests = db.tests
-    # new_test = { "data" : request }
-    # tests.insert(new_test)
-    # print(tests)
-    return HttpResponse('')
+    return HttpResponse('YAY')
 
 def testPath(request, path):
     return HttpResponse(path)
@@ -106,6 +107,3 @@ def printTest(request):
 
 def confirm(request, email, hashed):
     return redirect('/index.html?email=' + email + '&hash=' + hashed)
-    # confs = db.confs
-    # conf = confs.find_one({"email": email, "hash": hashed})
-
